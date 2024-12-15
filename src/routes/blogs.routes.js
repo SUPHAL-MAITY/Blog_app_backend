ad7dacda42eeb1e827078887b136f9edc6035532
SUPHAL-MAITY <suphalrohit@gmail.com>
2024-12-15 20:31:07 +0530

    add bakcend

---
+++ src/routes/blogs.routes.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+
+import { blogsByCategoriesController, changeBlogStatusController, createBlogcontroller, deleteBlogController, getAllBlogsController, getAllBlogsForUserController, getBlogsByPageController, getOwnBlogsController, getPopularBLogsController, getRecentBlogsController, getSingleBlogController, searchItemController, updateBlogController } from "../controllers/blog.controller.js";
+import { decodeToken,isAdmin } from "../middlewares/auth.middleware.js";
+import { get } from "mongoose";
+
+
+
+
+
+
+const router=Router()
+
+
+
+
+router.route("/create-blog").post(decodeToken,createBlogcontroller)
+router.route("/delete-blog/:id").delete(deleteBlogController)
+router.route("/update-blog/:id").post(updateBlogController)
+router.route("/all-blogs").get(decodeToken,isAdmin,getAllBlogsController)
+router.route("/recent").get(getRecentBlogsController)
+router.route("/blogs-by-page").get(getBlogsByPageController)
+router.route("/single-blog/:id").get(getSingleBlogController)
+router.route("/get-own-blogs").get(decodeToken, getOwnBlogsController)
+router.route("/change-blog-status").post(changeBlogStatusController)
+router.route("/all-user-blogs").get(getAllBlogsForUserController)
+router.route("/category/:category").get(blogsByCategoriesController)
+router.route("/search").get(searchItemController)
+router.route("/popular").get(getPopularBLogsController)
+
+
+
+
+export default router;
